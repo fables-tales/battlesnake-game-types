88644df0793a6ab9454bcf1be0590e7c57c7ea5e
Corey Alexander <coreyja@gmail.com>
2021-10-16 17:10:23 -0400

    Fix this method for getting snake body from the board

--- src/compact_representation/mod.rs
+++ src/compact_representation/mod.rs
@@ -1070,12 +1070,23 @@ impl<T: CellNum, const BOARD_SIZE: usize, const MAX_SNAKES: usize> SnakeBodyGett
     fn get_snake_body_vec(&self, snake_id: &Self::SnakeIDType) -> Vec<Self::NativePositionType> {
         let mut body = vec![];
         body.reserve(self.get_length(*snake_id).into());
-        let mut cur = Some(self.get_head_as_native_position(snake_id));
+        let head = self.get_head_as_native_position(snake_id);
+
+        let mut cur = Some(self.get_cell(head).get_tail_position(head).unwrap());
 
         while let Some(c) = cur {
             body.push(c);
+            if self.get_cell(c).is_double_stacked_piece() {
+                body.push(c);
+            }
+            if self.get_cell(c).is_triple_stacked_piece() {
+                body.push(c);
+                body.push(c);
+            }
             cur = self.get_cell(c).get_next_index();
         }
+
+        body.reverse();
 
         body
     }
